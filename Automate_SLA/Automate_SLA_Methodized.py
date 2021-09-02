1fe04494b72e3d2d051837d2f52c7d3886a601cd
calvinbrown32 <calvinbrown32@gmail.com>
2021-09-01 21:50:13 -0400

    Moved testing Code to it's own file

--- Automate_SLA/Automate_SLA_Methodized.py
+++ Automate_SLA/Automate_SLA_Methodized.py
@@ -197,45 +197,10 @@ MAKE SLA TRACKING SHEET
 
 agenda_df
 
-"""
-BELOW: TESTING TRY and EXCEPT. 
-    I want to learn how to use this feature in my code
-    I want to print the error but not halt the code. 
-    This will be designed to alert if directory already exists
-    
-    OTHER ITEMS to learn: Returning multiple items from function. 
-"""
+mac_agenda = r'/Users/calvindechicago/Desktop/SLA_test.txt'
 
 
-def test_try_except(agenda_pull):
-    
-    # Open the text file
-    #r+ = read/write access mode
-    agenda = open(agenda_pull, 'r+')
-    
-    #Readlines creates a list, where each index contains a line of text, which in this case is a single establishment. 
-    contents = agenda.readlines()
-    
-    # Close the text file
-    agenda.close()
-    
-    # This creates a dataframe where each row is a line from the agenda pull
-    agenda_df = pd.DataFrame(contents, columns=['line'])
-    
-    
-    desktop = os.path.expanduser("~/Desktop")
-    top_folder = 'Test_try_except'
-    filepath = os.path.join(desktop, top_folder)
-    try:
-        os.makedirs(filepath)
-    except Exception as e: 
-        print(e)
-        pass
-    return 0, 'a function can return numerous values and data', ['hello', 'why','a function can return numerous values and data'], agenda_df; 
-
-# +
-# USE THIS BRANCH
-# -
+make_sla_folders(mac_agenda)
 
 """
 Mac Pull - This section is to aid development and testing on a mac. 
@@ -245,8 +210,6 @@ This contains a path to an SLA pull on the mac for quick testing.
 
 mac_agenda = r'/Users/calvindechicago/Desktop/SLA_test.txt'
 
-
-make_sla_folders(mac_agenda)
 
 test_try_except(mac_agenda)[3]
 
